048e1f024c9ffc441bb6adfe97fc8b00bf0e5cb0
Ahmed-abulfateh <ahmed-bh91@live.com>
2026-04-15 21:04:16 +0300

    refactor: separate messaging from booking module

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,8 +3,6 @@ const {
   createBooking,
   getMyBookings,
   updateBookingStatus,
-  getBookingChat,
-  addBookingChatMessage,
 } = require('../controllers/bookingController');
 const verifyToken = require('../middleware/verify-token');
 
@@ -17,9 +15,5 @@ router.post('/', createBooking);
 router.get('/mine', getMyBookings);
 
 router.patch('/:id/status', updateBookingStatus);
-
-router.get('/:id/chat', getBookingChat);
-
-router.post('/:id/chat', addBookingChatMessage);
 
 module.exports = router;
